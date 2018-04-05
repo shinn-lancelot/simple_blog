521ee6d4e118cc64d8446385b95ae22f778ac1c9
shinn lancelot <945226793@qq.com>
2018-04-05 12:39:03 +0800

    1.文章模型完成，并生成迁移文件及数据表 2.后台数据表创建

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    article_title = models.CharField(max_length=300)
+    article_text = models.CharField(max_length=5000)
+    create_time = models.DateTimeField(True)
+    update_time = models.DateTimeField(True)
